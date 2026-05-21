4d8c6d548b45f14414804cbe8501b8cf81878f18
Justin M <dev@tad.sr>
2026-05-21 13:03:36 -0300

    chore: update pnpm-lock.yaml to version 9.0 and refactor getPageIcon utility

--- utils/getPageIcon.ts
+++ utils/getPageIcon.ts
@@ -1,14 +1,34 @@
 import { ExtendedRecordMap } from 'notion-types';
-import { PageIconImpl } from 'react-notion-x';
+import { getBlockIcon, isUrl } from 'notion-utils';
+
+function getPageBlock(recordMap: ExtendedRecordMap) {
+  const blocks = Object.values(recordMap.block);
+  return (
+    blocks.find((b) => b?.value?.type === 'page')?.value ??
+    blocks.find((b) => b?.value?.type === 'collection_view_page')?.value ??
+    recordMap.block[Object.keys(recordMap.block)[0]]?.value
+  );
+}
 
 export function getPageIcon(recordMap: ExtendedRecordMap, defaultIcon?: string) {
-  let icon = defaultIcon;
-  const pageBlock = recordMap.block[Object.keys(recordMap.block)[0]]?.value;
-
-  if (pageBlock) {
-    const elem = PageIconImpl({ block: pageBlock, defaultIcon });
-    icon = encodeURIComponent(elem.props.children.props.src);
+  const pageBlock = getPageBlock(recordMap);
+  if (!pageBlock) {
+    return defaultIcon;
   }
 
-  return icon;
+  const icon = getBlockIcon(pageBlock, recordMap)?.trim();
+  if (!icon) {
+    return defaultIcon;
+  }
+
+  if (isUrl(icon)) {
+    return encodeURIComponent(icon);
+  }
+
+  if (icon.startsWith('/icons/')) {
+    return encodeURIComponent(`https://www.notion.so${icon}?mode=light`);
+  }
+
+  // Emoji and other non-URL icons cannot be used in OG images
+  return defaultIcon;
 }
